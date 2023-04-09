5949290fd1b9c036aacd011aa5338fd82b13a13c
Negrodcc <113861567+Negrodcc@users.noreply.github.com>
2023-04-09 11:08:33 -0400

    Demo lista de batalla 2D con una serpiente

--- Scenes/Snake/snake.gd
+++ Scenes/Snake/snake.gd
@@ -1,41 +1,43 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
 var angulo = 0
 var loking = Vector2.RIGHT
 var up = Vector2.UP
+const JUMP_VELOCITY = -500
 @onready var animation_tree = $AnimationTree
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func rotar():
+	var sentido = Input.get_axis("rotar_izq","rotar_der")
+	rotation += 0.1 * sentido
+	loking = Vector2.from_angle(rotation)
+
 
 func _ready():
 	animation_tree.active = true
 
 func _physics_process(delta):
-	movement()
 	
-
-func movement():
+	if not is_on_floor():
+		rotar()
+		#gravedad
+		velocity.y += gravity * delta
 	
-	var sentido = Input.get_axis("rotar_izq","rotar_der")
-	rotation += 0.1 * sentido
+	if is_on_floor():
+		rotation = 0
+		loking = Vector2.from_angle(rotation)
 	
-	loking = Vector2.from_angle(rotation)
-	up = Vector2.from_angle(rotation-1.5707)
+	#movimiento normal
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	#saltar
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 	
-	var direction_X =  Input.get_axis("ui_left","ui_right")
-	var direction_Y = Input.get_axis("ui_down","ui_up")
-	
-	loking *= direction_X * SPEED
-	up *= direction_Y*SPEED
-	
-	velocity = loking + up
-	
-	
-	if Input.is_action_pressed("ui_accept"):
-		velocity = velocity.normalized()*4*SPEED
-		
 	move_and_slide()
-	
-func _process(delta):
-	pass
 
