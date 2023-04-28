4eac9d94f73782347650bab3568546cd9e677277
Negrodcc <113861567+Negrodcc@users.noreply.github.com>
2023-04-28 19:09:15 -0400

    Add Rotation of the snake when you change direction

--- Scenes/Snake/snake.gd
+++ Scenes/Snake/snake.gd
@@ -7,6 +7,9 @@ var up = Vector2.UP
 const JUMP_VELOCITY = -500
 @onready var animation_tree = $AnimationTree
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@onready var pivot = $Pivot
+@onready var hitbox = $CollisionShape2D
+@onready var posicion_hitbox = hitbox.get("position")
 
 func rotar():
 	var sentido = Input.get_axis("rotar_izq","rotar_der")
@@ -32,12 +35,25 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	#saltar
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if direction!=0:
+		pivot.scale.x = sign(direction)
+		if direction==-1:
+			hitbox.position.x = -1
+		
+		if direction==1:
+			hitbox.position.x = 0
+	
 	
 	move_and_slide()
+	
+
+	
 
