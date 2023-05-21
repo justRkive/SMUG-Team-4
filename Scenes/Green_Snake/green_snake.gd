7ab70ad0136fba2ac1c969ec86fb63cc767f2124
Negrodcc <113861567+Negrodcc@users.noreply.github.com>
2023-05-21 00:10:33 -0400

    Add another snake and the attack and die animation with funcionality

--- Scenes/Green_Snake/green_snake.gd
+++ Scenes/Green_Snake/green_snake.gd
@@ -1,28 +1,27 @@
 extends CharacterBody2D
 
-var permiso_atacar = true
 const SPEED = 200.0
+var permiso_atacar = true
 var angulo = 0
 var loking = Vector2.RIGHT
 var last_direction = 1
 var up = Vector2.UP
-var permiso_idle = true
 const JUMP_VELOCITY = -500
-@onready var hp = 20
+var  permiso_idle = true
+@onready var hp = 10
+@onready var animated_sprite_2d = $Pivot/AnimatedSprite2D
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var pivot = $Pivot
 @onready var hitbox = $CollisionShape2D
 @onready var posicion_hitbox = hitbox.get("position")
-#que pasa ahora si que si o no
 @onready var dead_timer = $DeadTimer
-@onready var animated_sprit_reed = $Pivot/AnimatedSpritReed
 @onready var attack_timer = $AttackTimer
 
-#Attack
 var BlueBall = preload("res://Utility/blue_attack.tscn")
 
+ 
 func rotar():
-	var sentido = Input.get_axis("rotar_izq","rotar_der")
+	var sentido = Input.get_axis("key_O","key_P")
 	rotation += 0.1 * sentido
 	loking = Vector2.from_angle(rotation)
 	
@@ -37,27 +36,26 @@ func attack():
 		pivot.rotation = loking.angle()
 		if rotation>1.6:
 			pivot.scale.x = sign(loking.x)
-		print(rotation)
 	add_child(blueball_attack)
 
+	
 
 func _ready():
 	pass
 
-	
 func _physics_process(delta):
 	
 	if hp>0 and permiso_idle:
-		animated_sprit_reed.play("idle")
+		animated_sprite_2d.play("idle")
 	
-	if Input.is_action_just_pressed("key_y"):
-		if permiso_atacar:
-			permiso_idle = false
-			animated_sprit_reed.play("attack")
-			permiso_atacar = false
-			attack_timer.wait_time = 1
-			attack_timer.start()
-			attack()
+		if Input.is_action_just_pressed("key_L"):
+			if permiso_atacar:
+				permiso_idle = false
+				animated_sprite_2d.play("attack")
+				permiso_atacar = false
+				attack_timer.wait_time = 1
+				attack_timer.start()
+				attack()
 		
 	if not is_on_floor():
 		rotar()
@@ -69,35 +67,38 @@ func _physics_process(delta):
 		loking = Vector2.from_angle(rotation)
 	
 	#movimiento normal
-	var direction = Input.get_axis("key_left", "key_right")
+	var direction = Input.get_axis("key_A", "key_D")
 	if direction:
 		velocity.x = direction * SPEED
 		loking = loking*direction
 		last_direction = direction
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		loking = loking*last_direction
 	
 	#saltar
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("key_W") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-	
+		
 	if direction!=0:
 		pivot.scale.x = sign(direction)
 		if direction==-1:
-			hitbox.position.x = -1
+			hitbox.position.x = -1.5
 		
 		if direction==1:
-			hitbox.position.x = 0
+			hitbox.position.x = 1.5
+	
 	
 	move_and_slide()
 	
-func _on_hurt_box_area_entered(area):
+
+func _on_hurt_box_green_area_entered(area):
 	if area.is_in_group("blue_ball"):
 		var damage = area.damage
 		hp-=damage
 		if hp<=0:
-			animated_sprit_reed.play("die")
+			animated_sprite_2d.play("die")
 			dead_timer.wait_time = 1
 			dead_timer.start()
 
@@ -105,6 +106,6 @@ func _on_hurt_box_area_entered(area):
 func _on_dead_timer_timeout():
 	queue_free()
 
+
 func _on_attack_timer_timeout():
 	permiso_idle = true
-	
